91f61cda72e52236338503c6561f7a03c5856936
hbuminy <pybhad@gmail.com>
2023-05-22 12:26:09 +0300

    yes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build:{
-    base:'portfolio',
+    base:'/portfolio/',
     outDir:'./docs'
   }
 })
